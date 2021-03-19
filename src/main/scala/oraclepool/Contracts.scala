9f2ece7d340d86156ab7647a719cd55c383165b1
scalahub <23208922+scalahub@users.noreply.github.com>
2021-03-19 19:03:23 +0530

    Add new voting and update contracts

--- src/main/scala/oraclepool/Contracts.scala
+++ src/main/scala/oraclepool/Contracts.scala
@@ -47,12 +47,12 @@ trait Contracts {
        |  // R6: Data point
        |
        |
-       |  val oracleBoxes = INPUTS.filter{(b:Box) =>
+       |  val oracleBoxes = CONTEXT.dataInputs.filter{(b:Box) =>
        |    b.R5[Coll[Byte]].get == SELF.id &&
        |    b.tokens(0)._1 == oracleTokenId
        |  }
        |
-       |  val pubKey = oracleBoxes.map{(b:Box) => proveDlog(b.R4[GroupElement].get)}(OUTPUTS(1).R5[Int].get) // 1st output will be a datapoint box, and its R4 is locked to public key. Hence we will use R5
+       |  val pubKey = oracleBoxes.map{(b:Box) => proveDlog(b.R4[GroupElement].get)}(OUTPUTS(1).R4[Int].get)
        |
        |  val sum = oracleBoxes.fold(0L, { (t:Long, b: Box) => t + b.R6[Long].get })
        |
@@ -62,15 +62,23 @@ trait Contracts {
        |
        |  def getPrevOracleDataPoint(index:Int) = if (index <= 0) firstOracleDataPoint else oracleBoxes(index - 1).R6[Long].get
        |
+       |  val firstOracleBoxIdPlusOne = byteArrayToBigInt(oracleBoxes(0).id) - 1
+       |  
+       |  def getPrevOracleBoxId(index:Int) = if (index <= 0) firstOracleBoxIdPlusOne else byteArrayToBigInt(oracleBoxes(index - 1).id)
+       |
        |  val rewardAndOrderingCheck = oracleBoxes.fold((1, true), {
        |      (t:(Int, Boolean), b:Box) =>
        |         val currOracleDataPoint = b.R6[Long].get
        |         val prevOracleDataPoint = getPrevOracleDataPoint(t._1 - 1)
+       |         val prevOracleBoxId = getPrevOracleBoxId(t._1 - 1) 
+       |         val currOracleBoxId = byteArrayToBigInt(b.id)
+       |         val validOrderById = if (prevOracleDataPoint == currOracleDataPoint) prevOracleBoxId < currOracleBoxId else true 
+       |          
        |         (t._1 + 1, t._2 &&
-       |                    OUTPUTS(t._1).propositionBytes == b.propositionBytes &&
-       |                    OUTPUTS(t._1).R4[GroupElement].get == b.R4[GroupElement].get &&
-       |                    OUTPUTS(t._1).value >= b.value + $oracleReward &&
-       |                    prevOracleDataPoint >= currOracleDataPoint 
+       |                    OUTPUTS(t._1).propositionBytes == proveDlog(b.R4[GroupElement].get).propBytes &&
+       |                    OUTPUTS(t._1).value >= $oracleReward &&
+       |                    prevOracleDataPoint >= currOracleDataPoint &&
+       |                    validOrderById
        |         )
        |     }
        |  )
@@ -116,32 +124,34 @@ trait Contracts {
        |                            blake2b256(OUTPUTS(0).propositionBytes) == liveEpochScriptHash
        |    ( // start next epoch
        |      epochNotOver && canStartEpoch && enoughFunds &&
-       |      OUTPUTS(0).R4[Long].get == SELF.R4[Long].get &&
-       |      OUTPUTS(0).R5[Int].get == SELF.R5[Int].get &&
        |      OUTPUTS(0).tokens == SELF.tokens &&
        |      OUTPUTS(0).value >= SELF.value &&
+       |      OUTPUTS(0).R4[Long].get == SELF.R4[Long].get &&
+       |      OUTPUTS(0).R5[Int].get == SELF.R5[Int].get &&
        |      isliveEpochOutput
        |    ) || ( // create new epoch
        |      epochOver &&
        |      enoughFunds &&
+       |      OUTPUTS(0).tokens == SELF.tokens &&
+       |      OUTPUTS(0).value >= SELF.value &&
+       |      OUTPUTS(0).tokens == SELF.tokens &&
+       |      OUTPUTS(0).value >= SELF.value &&
        |      OUTPUTS(0).R4[Long].get == SELF.R4[Long].get &&
        |      OUTPUTS(0).R5[Int].get >= minNewEpochHeight &&
        |      OUTPUTS(0).R5[Int].get <= maxNewEpochHeight &&
-       |      OUTPUTS(0).tokens == SELF.tokens &&
-       |      OUTPUTS(0).value >= SELF.value &&
        |      isliveEpochOutput
        |    )
        |  } else {
        |    ( // collect funds
-       |      OUTPUTS(0).R4[Long].get == SELF.R4[Long].get &&
-       |      OUTPUTS(0).R5[Int].get == SELF.R5[Int].get &&
        |      OUTPUTS(0).propositionBytes == SELF.propositionBytes &&
        |      OUTPUTS(0).tokens == SELF.tokens &&
-       |      OUTPUTS(0).value > SELF.value
+       |      OUTPUTS(0).value > SELF.value &&
+       |      OUTPUTS(0).R4[Long].get == SELF.R4[Long].get &&
+       |      OUTPUTS(0).R5[Int].get == SELF.R5[Int].get 
        |    )
        |  }
        |  
-       |  val updateAction = INPUTS(1).tokens(0)._1 == updateNFT && CONTEXT.dataInputs.size == 0
+       |  val updateAction = INPUTS(0).tokens(0)._1 == updateNFT 
        |  
        |  sigmaProp(poolAction || updateAction)
        |}
@@ -154,29 +164,27 @@ trait Contracts {
        |  // R4: The address of the oracle (never allowed to change after bootstrap).
        |  // R5: The box id of the latest Live Epoch box.
        |  // R6: The oracle's datapoint.
-       |  
-       |  // We will NOT have any box in data inputs during commit. So the owner is free to put any value into the registers. 
-       |  // The requirement of R5 having box id is checked in the collect datapoint action
-       |  // Specifically the datapoint collection will check
-       |  //  1. SELF.R5[Coll[Byte]].get == liveEpochBox.id
-       |  //  2. SELF.R6[Long].get is a long value (its sign can be anything, since the consensus enforces correctness)
-       |  
+       |
        |  val pubKey = SELF.R4[GroupElement].get
        |
-       |  def isBasicCopy(b:Box) = b.R4[GroupElement].get == pubKey && 
-       |                           b.propositionBytes == SELF.propositionBytes &&
-       |                           b.tokens == SELF.tokens
-       |                            
-       |  def isCollectOut(b:Box) = isBasicCopy(b) && b.value > SELF.value
-       |  
-       |  val isCollect = INPUTS(0).tokens(0)._1 == poolNFT && OUTPUTS.exists(isCollectOut)
-       |   
-       |  val isCommit = proveDlog(pubKey) && OUTPUTS.exists(isBasicCopy) // committer should ensure that in addition to isBasicCopy, the output should be of correct form 
-       |  
+       |  val poolBox = CONTEXT.dataInputs(0)
+       |
+       |  // Allow datapoint box to contain box id of any box with pool NFT (i.e., either Live Epoch or Epoch Prep boxes)
+       |  // Earlier we additionally required that the box have the live epoch script.
+       |  // In summary:
+       |  //    Earlier: (1st data-input has pool NFT) && (1st data-input has live epoch script) 
+       |  //    Now:     (1st data-input has pool NFT) 
+       |  //
+       |  val validPoolBox = poolBox.tokens(0)._1 == poolNFT 
+       | 
        |  sigmaProp(
-       |    isCollect || 
-       |    isCommit // can also extract accumulated Ergs here
-       |  )
+       |    OUTPUTS(0).R4[GroupElement].get == pubKey &&
+       |    OUTPUTS(0).R5[Coll[Byte]].get == poolBox.id &&
+       |    OUTPUTS(0).R6[Long].get > 0 &&
+       |    OUTPUTS(0).propositionBytes == SELF.propositionBytes &&
+       |    OUTPUTS(0).tokens == SELF.tokens &&
+       |    validPoolBox
+       |  ) && proveDlog(pubKey)
        |}
        |""".stripMargin
 
@@ -200,46 +208,78 @@ trait Contracts {
 
   env.setCollByte("ballotTokenId", ballotToken.decodeHex)
 
+  val ballotScript =
+    s"""{ // This box (ballot box):
+       |  // R4 the group element of the owner of the ballot token [GroupElement]
+       |  // R5 dummy Int due to AOTC non-lazy evaluation (since pool box has Int at R5). Due to the line marked ****
+       |  // R6 the box id of the update box [Coll[Byte]]
+       |  // R7 the value voted for [Coll[Byte]]
+       |  
+       |  val pubKey = SELF.R4[GroupElement].get
+       |  
+       |  val index = INPUTS.indexOf(SELF, 0)
+       |  
+       |  val output = OUTPUTS(index)
+       |  
+       |  val isBasicCopy = output.R4[GroupElement].get == pubKey && 
+       |                    output.propositionBytes == SELF.propositionBytes &&
+       |                    output.tokens == SELF.tokens && 
+       |                    output.value >= $minStorageRent
+       |  
+       |  sigmaProp(
+       |    isBasicCopy && (
+       |      proveDlog(pubKey) || (
+       |         INPUTS(0).tokens(0)._1 == updateNFT && 
+       |         output.value >= SELF.value
+       |      )
+       |    )
+       |  )
+       |}
+       |""".stripMargin
+
   val updateScript =
-    s"""{ // This box:
+    s"""{ // This box (update box):
        |  // Registers empty 
        |  // 
        |  // ballot boxes (Inputs)
-       |  // R4 the new control value
-       |  // R5 the box id of this box
+       |  // R4 the pub key of voter [GroupElement] (not used here)
+       |  // R5 dummy int due to AOTC non-lazy evaluation (from the line marked ****)
+       |  // R6 the box id of this box [Coll[Byte]]
+       |  // R7 the value voted for [Coll[Byte]]
        |
-       |  // collect and update in one step now!
-       |  val poolBoxIn = INPUTS(0) // pool is 1st input
+       |  // collect and update in one step
+       |  val updateBoxOut = OUTPUTS(0) // copy of this box is the 1st output
+       |  val validUpdateIn = SELF.id == INPUTS(0).id // this is 1st input
+       |
+       |  val poolBoxIn = INPUTS(1) // pool box is 2nd input
+       |  val poolBoxOut = OUTPUTS(1) // copy of pool box is the 2nd output
        |  
-       |  val poolBoxOut = OUTPUTS(0) // pool is the 1st output
-       |  val updateBoxOut = OUTPUTS(1)
-       |  
+       |  // compute the hash of the pool output box. This should be the value voted for
        |  val poolBoxOutHash = blake2b256(poolBoxOut.propositionBytes)
        |  
        |  val validPoolIn = poolBoxIn.tokens(0)._1 == poolNFT
        |  val validPoolOut = poolBoxIn.tokens == poolBoxOut.tokens && 
+       |                     poolBoxIn.value == poolBoxOut.value &&
        |                     poolBoxIn.R4[Long].get == poolBoxOut.R4[Long].get &&
-       |                     poolBoxIn.R5[Int].get == poolBoxOut.R5[Int].get &&
-       |                     poolBoxIn.value == poolBoxOut.value
+       |                     poolBoxIn.R5[Int].get == poolBoxOut.R5[Int].get 
        |
-       |  
-       |  val validUpdateIn = SELF.id == INPUTS(1).id // this is 2nd input
        |  
        |  val validUpdateOut = SELF.tokens == updateBoxOut.tokens && 
        |                       SELF.propositionBytes == updateBoxOut.propositionBytes &&
        |                       SELF.value >= updateBoxOut.value
        |
-       |  def validBallotSubmissionBox(b:Box) = b.tokens(0)._1 == ballotTokenId &&
-       |                                        b.R4[Coll[Byte]].get == poolBoxOutHash && // ensure that vote is for the poolBoxOutHash
-       |                                        b.R5[Coll[Byte]].get == SELF.id  // ensure that vote corresponds to this update only
+       |  def isValidBallot(b:Box) = {
+       |    b.tokens.size > 0 && 
+       |    b.tokens(0)._1 == ballotTokenId &&
+       |    b.R6[Coll[Byte]].get == SELF.id && // ensure vote corresponds to this box ****
+       |    b.R7[Coll[Byte]].get == poolBoxOutHash // check value voted for
+       |  }
        |  
-       |  val ballotsIn = INPUTS.filter(validBallotSubmissionBox)
+       |  val ballotBoxes = INPUTS.filter(isValidBallot)
        |  
-       |  val ballotCount = ballotsIn.fold(0L, { (accum: Long, box: Box) =>  accum + box.tokens(0)._2 })
+       |  val votesCount = ballotBoxes.fold(0L, {(accum: Long, b: Box) => accum + b.tokens(0)._2})
        |  
-       |  val voteAccepted = ballotCount >= $minVotes
-       |
-       |  sigmaProp(validPoolIn && validPoolOut && validUpdateIn && validUpdateOut && voteAccepted)
+       |  sigmaProp(validPoolIn && validPoolOut && validUpdateIn && validUpdateOut && votesCount >= $minVotes)
        |}
        |""".stripMargin
 
@@ -252,10 +292,12 @@ trait Contracts {
   env.setCollByte("epochPrepScriptHash", Blake2b256(epochPrepErgoTree.bytes))
   val poolDepositErgoTree: Values.ErgoTree = ScriptUtil.compile(env.toMap, poolDepositScript)
   val updateErgoTree: Values.ErgoTree = ScriptUtil.compile(env.toMap, updateScript)
+  val ballotErgoTree: Values.ErgoTree = ScriptUtil.compile(env.toMap, ballotScript)
 
   val liveEpochAddress: String = getStringFromAddress(getAddressFromErgoTree(liveEpochErgoTree))
   val epochPrepAddress: String = getStringFromAddress(getAddressFromErgoTree(epochPrepErgoTree))
   val dataPointAddress: String = getStringFromAddress(getAddressFromErgoTree(dataPointErgoTree))
   val poolDepositAddress: String = getStringFromAddress(getAddressFromErgoTree(poolDepositErgoTree))
   val updateAddress: String = getStringFromAddress(getAddressFromErgoTree(updateErgoTree))
+  val ballotAddress: String = getStringFromAddress(getAddressFromErgoTree(ballotErgoTree))
 }
--- src/test/scala/oraclepool/UpdateSpec.scala
+++ src/test/scala/oraclepool/UpdateSpec.scala
@@ -1,12 +1,16 @@
 package oraclepool
 
+import kiosk.ErgoUtil
 import kiosk.encoding.ScalaErgoConverters
+import kiosk.encoding.ScalaErgoConverters.{getAddressFromErgoTree, getStringFromAddress, stringToGroupElement}
 import kiosk.ergo._
 import kiosk.tx.TxUtil
 import org.ergoplatform.appkit._
 import org.scalatest.{Matchers, PropSpec}
 import org.scalatestplus.scalacheck.ScalaCheckDrivenPropertyChecks
 import scorex.crypto.hash.Blake2b256
+import sigmastate.lang.exceptions.InterpreterException
+import special.sigma.GroupElement
 
 class UpdateSpec extends PropSpec with Matchers with ScalaCheckDrivenPropertyChecks with HttpClientTesting {
   /*
@@ -26,84 +30,232 @@ class UpdateSpec extends PropSpec with Matchers with ScalaCheckDrivenPropertyChe
 
   property("Update") {
     ergoClient.execute { implicit ctx: BlockchainContext =>
-      val poolBoxIn = 2518217000L
-
-      val epochPool = new OraclePool {}
-      val newOraclePool = new OraclePool {
+      val currentPool = new OraclePool {}
+      val newPool = new OraclePool {
         override lazy val maxNumOracles = 13
         override def livePeriod = 12 // blocks          CHANGED!!
         override def prepPeriod = 6 // blocks           CHANGED!!
         override def buffer = 4 // blocks               CHANGED!!
       }
 
-      require(epochPool.epochPrepAddress != newOraclePool.epochPrepAddress)
+//      require(currentPool.epochPrepAddress != newPool.epochPrepAddress)
+      require(currentPool.liveEpochAddress != newPool.liveEpochAddress)
 
-      require(epochPool.liveEpochAddress != newOraclePool.liveEpochAddress)
+      require(currentPool.poolNFT == newPool.poolNFT)
+      require(currentPool.updateNFT == newPool.updateNFT)
+      require(currentPool.ballotToken == newPool.ballotToken)
+      require(currentPool.oracleToken == newPool.oracleToken)
 
-      // datapoint script should not change
-      require(epochPool.dataPointAddress == newOraclePool.dataPointAddress)
+      require(currentPool.updateAddress == newPool.updateAddress)
+      require(currentPool.ballotAddress == newPool.ballotAddress)
+      require(currentPool.dataPointAddress == newPool.dataPointAddress)
+      require(currentPool.poolDepositAddress == newPool.poolDepositAddress)
+
       // dummy custom input box for funding various transactions
       val dummyFundingBox = ctx
         .newTxBuilder()
         .outBoxBuilder
-        .value(fee)
+        .value(100000000000L)
         .registers(KioskCollByte(Array(1)).getErgoValue)
         .contract(ctx.compileContract(ConstantsBuilder.empty(), dummyScript))
         .build()
         .convertToInputWith(dummyTxId, 0)
 
-      // old pool box
-      val epochPrepBoxIn = ctx
+      object Voters {
+        // define voters
+        val addresses = Seq(
+          "9eiuh5bJtw9oWDVcfJnwTm1EHfK5949MEm5DStc2sD1TLwDSrpx", // private key is 37cc5cb5b54f98f92faef749a53b5ce4e9921890d9fb902b4456957d50791bd0
+          "9f9q6Hs7vXZSQwhbrptQZLkTx15ApjbEkQwWXJqD2NpaouiigJQ", // private key is 5878ae48fe2d26aa999ed44437cffd2d4ba1543788cff48d490419aef7fc149d
+          "9fGp73EsRQMpFC7xaYD5JFy2abZeKCUffhDBNbQVtBtQyw61Vym", // private key is 3ffaffa96b2fd6542914d3953d05256cd505d4beb6174a2601a4e014c3b5a78e
+        ).toArray
+
+        val privateKey0 = "37cc5cb5b54f98f92faef749a53b5ce4e9921890d9fb902b4456957d50791bd0"
+        val privateKey1 = "5878ae48fe2d26aa999ed44437cffd2d4ba1543788cff48d490419aef7fc149d"
+        val privateKey2 = "3ffaffa96b2fd6542914d3953d05256cd505d4beb6174a2601a4e014c3b5a78e"
+
+        val r4voter0 = KioskGroupElement(stringToGroupElement(ErgoUtil.addressToGroupElement(addresses(0))))
+        val r4voter1 = KioskGroupElement(stringToGroupElement(ErgoUtil.addressToGroupElement(addresses(1))))
+        val r4voter2 = KioskGroupElement(stringToGroupElement(ErgoUtil.addressToGroupElement(addresses(2))))
+
+        val ballot0Box = KioskBox(currentPool.ballotAddress, value = 200000000, registers = Array(r4voter0), tokens = Array((currentPool.ballotToken, 3L)))
+        val ballot1Box = KioskBox(currentPool.ballotAddress, value = 200000000, registers = Array(r4voter1), tokens = Array((currentPool.ballotToken, 4L)))
+        val ballot2Box = KioskBox(currentPool.ballotAddress, value = 200000000, registers = Array(r4voter2), tokens = Array((currentPool.ballotToken, 1L)))
+      }
+
+      // old update box
+      val updateOutBox = ctx
         .newTxBuilder()
         .outBoxBuilder
-        .value(poolBoxIn)
-        .tokens(
-          new ErgoToken(epochPool.poolNFT, 1)
-        )
-        .contract(ctx.newContract(ScalaErgoConverters.getAddressFromString(epochPool.epochPrepAddress).script))
+        .value(currentPool.minStorageRent)
+        .tokens(new ErgoToken(currentPool.updateNFT, 1))
+        .contract(ctx.newContract(ScalaErgoConverters.getAddressFromString(currentPool.updateAddress).script))
+        .build()
+
+      val updateBoxIn = updateOutBox.convertToInputWith(dummyTxId, 0)
+
+      // old pool box
+      val currentEpochPrepContract = ctx.newContract(ScalaErgoConverters.getAddressFromString(currentPool.epochPrepAddress).script)
+
+      val poolBoxIn = ctx
+        .newTxBuilder()
+        .outBoxBuilder
+        .value(10000000)
+        .tokens(new ErgoToken(currentPool.poolNFT, 1))
+        .contract(currentEpochPrepContract)
         .registers(KioskLong(100).getErgoValue, KioskInt(100).getErgoValue)
         .build()
         .convertToInputWith(dummyTxId, 0)
 
-      // new pool box
-      val epochPrepBoxOut = KioskBox(
-        newOraclePool.epochPrepAddress,
-        value = epochPrepBoxIn.getValue,
-        registers = Array(KioskLong(100), KioskInt(100)),
-        tokens = Array(epochPool.poolNFT -> 1L)
+      // for for update
+      val valueVotedFor = KioskCollByte(Blake2b256(newPool.epochPrepErgoTree.bytes))
+
+      val ballot0BoxToCreate = Voters.ballot0Box.copy(
+        registers = Array(
+          Voters.ballot0Box.registers(0),
+          KioskInt(0), // dummy value due to AOTC non-lazy eval bug
+          KioskCollByte(updateBoxIn.getId.getBytes),
+          valueVotedFor
+        )
       )
 
-      val updatedPoolScriptHash = KioskCollByte(Blake2b256(newOraclePool.epochPrepErgoTree.bytes))
+      val ballot1BoxToCreate = Voters.ballot1Box.copy(
+        registers = Array(
+          Voters.ballot1Box.registers(0),
+          KioskInt(0), // dummy value due to AOTC non-lazy eval bug
+          KioskCollByte(updateBoxIn.getId.getBytes),
+          valueVotedFor
+        )
+      )
 
-      // old update box
-      val updateBoxIn = ctx
-        .newTxBuilder()
-        .outBoxBuilder
-        .value(10000000)
-        .tokens(new ErgoToken(epochPool.updateNFT, 1))
-        .contract(ctx.newContract(ScalaErgoConverters.getAddressFromString(epochPool.updateAddress).script))
-        .registers(updatedPoolScriptHash.getErgoValue)
-        .build()
-        .convertToInputWith(dummyTxId, 0)
+      val ballot2BoxToCreate = Voters.ballot2Box.copy(
+        registers = Array(
+          Voters.ballot2Box.registers(0),
+          KioskInt(0), // dummy value due to AOTC non-lazy eval bug
+          KioskCollByte(updateBoxIn.getId.getBytes),
+          valueVotedFor
+        )
+      )
+
+      // create ballots
+      val ballot0 = TxUtil.createTx(
+        inputBoxes = Array(Voters.ballot0Box.toInBox(dummyTxId, 0), dummyFundingBox),
+        dataInputs = Array(),
+        boxesToCreate = Array(ballot0BoxToCreate),
+        fee,
+        changeAddress,
+        proveDlogSecrets = Array[String](Voters.privateKey0),
+        Array[DhtData](),
+        false
+      ).getOutputsToSpend.get(0)
+
+      val ballot1 = TxUtil.createTx(
+        inputBoxes = Array(Voters.ballot1Box.toInBox(dummyTxId, 0), dummyFundingBox),
+        dataInputs = Array(),
+        boxesToCreate = Array(ballot1BoxToCreate),
+        fee,
+        changeAddress,
+        proveDlogSecrets = Array[String](Voters.privateKey1),
+        Array[DhtData](),
+        false
+      ).getOutputsToSpend.get(0)
+
+      val ballot2 = TxUtil.createTx(
+        inputBoxes = Array(Voters.ballot2Box.toInBox(dummyTxId, 0), dummyFundingBox),
+        dataInputs = Array(),
+        boxesToCreate = Array(ballot2BoxToCreate),
+        fee,
+        changeAddress,
+        proveDlogSecrets = Array[String](Voters.privateKey2),
+        Array[DhtData](),
+        false
+      ).getOutputsToSpend.get(0)
+
+      // voting should fail with wrong private key
+      the[AssertionError] thrownBy {
+        TxUtil.createTx(
+          inputBoxes = Array(Voters.ballot2Box.toInBox(dummyTxId, 0), dummyFundingBox),
+          dataInputs = Array(),
+          boxesToCreate = Array(ballot2BoxToCreate),
+          fee,
+          changeAddress,
+          proveDlogSecrets = Array[String](Voters.privateKey0),
+          Array[DhtData](),
+          false
+        )
+      } should have message "assertion failed: Tree root should be real but was UnprovenSchnorr(ProveDlog((6357df04d57e9d4d0564e217a0e7e6d201df72787b5f89203744e9384402378d,567526ec80c82cca6bbb6d5324e65c6cf9b39704ad91e3eca93f84bf481bf38a,1)),None,None,None,true,0)"
 
       // new update box
       val updateBoxOut = KioskBox(
-        epochPool.updateAddress,
+        currentPool.updateAddress,
         value = updateBoxIn.getValue,
-        registers = Array(updatedPoolScriptHash),
-        tokens = Array(epochPool.updateNFT -> 1L)
+        registers = Array(),
+        tokens = Array(currentPool.updateNFT -> 1L)
       )
 
+      // new pool box
+      val poolBoxOut = KioskBox(
+        newPool.epochPrepAddress,
+        value = poolBoxIn.getValue,
+        registers = Array(KioskLong(100), KioskInt(100)),
+        tokens = Array(currentPool.poolNFT -> 1L)
+      )
+
+      // Should succeed with sufficient votes (8)
       TxUtil.createTx(
-        inputBoxes = Array(epochPrepBoxIn, updateBoxIn, dummyFundingBox),
+        inputBoxes = Array(updateBoxIn, poolBoxIn, ballot0, ballot1, ballot2, dummyFundingBox),
         dataInputs = Array(),
-        boxesToCreate = Array(epochPrepBoxOut, updateBoxOut),
+        boxesToCreate = Array(updateBoxOut, poolBoxOut, ballot0BoxToCreate, ballot1BoxToCreate, ballot2BoxToCreate),
         fee,
         changeAddress,
-        Array[String](),
+        proveDlogSecrets = Array[String](),
         Array[DhtData](),
         false
       )
+
+      // should fail for invalid output pool box script
+      the[Exception] thrownBy {
+        TxUtil.createTx(
+          inputBoxes = Array(updateBoxIn, poolBoxIn, ballot0, ballot1, ballot2, dummyFundingBox),
+          dataInputs = Array(),
+          boxesToCreate = Array(updateBoxOut, poolBoxOut.copy(address = currentPool.epochPrepAddress), ballot0BoxToCreate, ballot1BoxToCreate, ballot2BoxToCreate),
+          fee,
+          changeAddress,
+          proveDlogSecrets = Array[String](),
+          Array[DhtData](),
+          false
+        )
+      } should have message "Script reduced to false"
+
+      // should fail for invalid input update box Id
+      val invalidUpdateBoxIn = updateOutBox.convertToInputWith(dummyTxId, 1) // different outputIndex
+
+      the[Exception] thrownBy {
+        TxUtil.createTx(
+          inputBoxes = Array(invalidUpdateBoxIn, poolBoxIn, ballot0, ballot1, ballot2, dummyFundingBox),
+          dataInputs = Array(),
+          boxesToCreate = Array(updateBoxOut, poolBoxOut.copy(address = currentPool.epochPrepAddress), ballot0BoxToCreate, ballot1BoxToCreate, ballot2BoxToCreate),
+          fee,
+          changeAddress,
+          proveDlogSecrets = Array[String](),
+          Array[DhtData](),
+          false
+        )
+      } should have message "Script reduced to false"
+
+      // Should fail when having insufficient votes (7)
+      the[Exception] thrownBy {
+        TxUtil.createTx(
+          inputBoxes = Array(updateBoxIn, poolBoxIn, ballot0, ballot1, dummyFundingBox),
+          dataInputs = Array(),
+          boxesToCreate = Array(updateBoxOut, poolBoxOut, ballot0BoxToCreate, ballot1BoxToCreate),
+          fee,
+          changeAddress,
+          proveDlogSecrets = Array[String](),
+          Array[DhtData](),
+          false
+        )
+      } should have message "Script reduced to false"
+
     }
   }
 }
