ef86d770c36ff8d62142990081dc2e16da7b6f9b
ScalaHub <23208922+scalahub@users.noreply.github.com>
2021-03-21 05:15:49 +0530

    Update OraclePool.scala

--- src/main/scala/oraclepool/OraclePool.scala
+++ src/main/scala/oraclepool/OraclePool.scala
@@ -12,31 +12,6 @@ trait OraclePool extends Contracts {
   lazy val minVotes = 8 // for update
   lazy val minStorageRent = 10000000L
 
-  /*
-    V0
-    poolNFT           008a94c8c76bbaa1f0a346697d1794eb31d94b37e5533af9cc0b6932bf159339
-    12 oracle Tokens  5579de48d16e54ddb34df789d418e1f10c119e15a824ea32dc21696c067f9fbe
-    updateNFT         7b8e292c4a89efb509c89b10111468223678e0a855b20607a0b9fce80a9af694
-    12 ballot tokens  f502b38d40208f8faf0e4dd9f74952e17b24d8f96abb9c9f57f8e961d25a8e37
-
-
-  lazy val poolNFT = "c3f12f8055e697391d8173d867ddb2f6fc7000165bf386cd7705df95fd893b9f" // 1 token issued
-  lazy val oracleToken = "834c46884299abecb5caab08a04e95b7303436672507daeef703b1b0cc22e2d4" // 15 tokens issued
-  lazy val updateNFT = "551023bc00000965340153f6a602ff28b877b9569d4438ae35504d49f0aee77d" // 1 token issued
-  lazy val ballotToken = "8f3cee01b8c34cb9d36fd4706dd6c0762e4dba4ed239a22ecbedce8d604c1833" // 15 tokens issued
-
-
-new oracle pool launched ids
-==========================================
-PoolNFT 011d3364de07e5a26f0c4eef0852cddb387039a921b7154ef3cab22c6eda887f (quantity 1)
-==========================================
-Participant Token 8c27dd9d8a35aac1e3167d58858c0a8b4059b277da790552e37eba22df9b9035: 15
-==========================================
-UpdateNFT 720978c041239e7d6eb249d801f380557126f6324e12c5ba9172d820be2e1dde (quantity 1)
-==========================================
-Ballot token 053fefab5477138b760bc7ae666c3e2b324d5ae937a13605cb766ec5222e5518: 15
-   */
-
   lazy val poolNFT = "011d3364de07e5a26f0c4eef0852cddb387039a921b7154ef3cab22c6eda887f" // 1 token issued
   lazy val oracleToken = "8c27dd9d8a35aac1e3167d58858c0a8b4059b277da790552e37eba22df9b9035" // 15 tokens issued
   lazy val updateNFT = "720978c041239e7d6eb249d801f380557126f6324e12c5ba9172d820be2e1dde" // 1 token issued
